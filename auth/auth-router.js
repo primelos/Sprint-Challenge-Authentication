ca34e2c23d48f6b761119ff68f7fffd0e9d9b507
Ale <alearanibar@gmail.com>
2019-12-20 08:43:58 -0800

    token

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -8,4 +8,16 @@ router.post('/login', (req, res) => {
   // implement login
 });
 
+function createToken(user) {
+  const payload = {
+    id: user.id,
+    username: user.username
+  };
+  const secret = process.env.JWT_SECRET || 'This is a secret';
+  const options = {
+    expiresIn: '1hr'
+  }
+  return jwt.sign(payload, secret, options);
+};
+
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -7,19 +7,20 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   res.status(401).json({ you: 'shall not pass!' });
     const { authorization } = req.headers;
-    console.log(authorization)
+    console.log(`authorization`, authorization)
 
   if (authorization) {
     const secret = process.env.JWT_SECRET || 'YoU CaN not HacK tHis';
     
     jwt.verify(authorization, secret, function(err, decodedToken) {
       if(err) {
+        console.log(`err`, err)
         req.token = decodedToken;
-        console.log(req.token)
+        console.log(`req.token`,req.token)
         next()
       }
     })
   } else {
-    res.status(403).json({ message: "Please login and try again"})
+    res.status(400).json({ message: "Please login and try again"})
   }
 };
