8ba78a3bfdc6746698581c25c9c223bc52bee3a4
Ale <alearanibar@gmail.com>
2019-12-20 08:36:15 -0800

    authenticate-middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,24 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   res.status(401).json({ you: 'shall not pass!' });
+    const { authorization } = req.headers;
+    console.log(authorization)
+
+  if (authorization) {
+    const secret = process.env.JWT_SECRET || 'YoU CaN not HacK tHis';
+    
+    jwt.verify(authorization, secret, function(err, decodedToken) {
+      if(err) {
+        req.token = decodedToken;
+        console.log(req.token)
+        next()
+      }
+    })
+  } else {
+    res.status(403).json({ message: "Please login and try again"})
+  }
 };
